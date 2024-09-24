5a178490016e94c9bcaa98474e81c05ba69f3c62
azimy-hamid <hamidazimy875@gmail.com>
2024-09-24 16:25:55 +0800

    Created authentication middleware in the backend -- Not tested yet

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 import * as taskController from "../controllers/todoTaskController.js";
+import authenticate from "../middlewares/authenticate.js";
+
 const taskRoutes = express.Router();
 
-taskRoutes.get("/getAllTask", taskController.getAllTasks);
-taskRoutes.post("/create", taskController.createTask);
-taskRoutes.put("/updateTask/:taskId", taskController.updateTask);
-taskRoutes.delete("/deleteTask/:taskId", taskController.deleteTask);
+taskRoutes.get("/getAllTask", authenticate, taskController.getAllTasks);
+taskRoutes.post("/create", authenticate, taskController.createTask);
+taskRoutes.put("/updateTask/:taskId", authenticate, taskController.updateTask);
+taskRoutes.delete(
+  "/deleteTask/:taskId",
+  authenticate,
+  taskController.deleteTask
+);
 
 export default taskRoutes;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import * as userController from "../controllers/userController.js";
+import authenticate from "../middlewares/authenticate.js";
 const userRoutes = express.Router();
 
 userRoutes.post("/register", userController.registerUser);
 userRoutes.post("/login", userController.loginUser);
-userRoutes.put("/updateDetails", userController.updateUserDetails);
-userRoutes.delete("/deleteUser", userController.hideUser);
+userRoutes.put(
+  "/updateDetails",
+  authenticate,
+  userController.updateUserDetails
+);
+userRoutes.delete("/deleteUser", authenticate, userController.hideUser);
 
 export default userRoutes;
